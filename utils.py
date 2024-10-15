f08421ddca6af3aa1d8ffe6ac4a319733a529cf1
Arseney Abraamyan <ar.abraamyan@gmail.com>
2024-10-15 23:01:51 +0300

    utils update add_words

--- utils.py
+++ utils.py
@@ -5,7 +5,11 @@ import json
 def get_words(file_name='words.json'):
     with open(file_name, 'r') as f:
         return json.load(f)
-    
+
+def add_words(updated_words, file_name='words.json'):
+    with open(file_name, 'w', encoding='utf-8') as f:
+        json.dump((updated_words), f, ensure_ascii=False, indent=4)
+
 def save_words(updated_words, file_name='words.json'):
     with open(file_name, 'w', encoding='utf-8') as f:
         json.dump(dict(sorted(updated_words.items())), f, ensure_ascii=False, indent=4)
